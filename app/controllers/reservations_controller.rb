3762e2be8b1ae5bc54d23181b2f08705d1669924
yamagishi <spica_sigsig@yahoo.co.jp>
2022-12-10 09:37:52 +0000

    微調整

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,18 @@
 class ReservationsController < ApplicationController
   def create
     if @reservation = current_user.reservations.create(reservation_params)
-      redirect_to "show"
+      start_date = Date.parse(reservation_params[:start_date])
+      end_date = Date.parse(reservation_params[:end_date])
+      days = (end_date - start_date).to_i + 1
+  
+      @reservation = current_user.reservations.build(reservation_params)
+      @reservation.post = post
+      @reservation.price = post.price
+      @reservation.total = post.price * days * person_num
+      @reservation.save
+  
+      flash[:notice] = "予約が完了しました。"
+      redirect_to "/show"
     else
       render show_post_path
     end
@@ -14,7 +25,7 @@ class ReservationsController < ApplicationController
   private
 
  def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date, :book_id)
+    params.require(:reservation).permit(:start_date, :end_date, :post_id)
   end 
   
 end
